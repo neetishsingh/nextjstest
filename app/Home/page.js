e433d63d8bfce2e03847977a6ca5dc24d02c5ee0
Neetish Singh <neetishsingh97@gmail.com>
2024-02-11 19:15:41 +0530

    decscription change

--- app/Home/page.js
+++ app/Home/page.js
@@ -120,7 +120,7 @@ export default function HomePage() {
           </div>
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Data to enrich your online business
+              Smart AI Based career manager..
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
               Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
